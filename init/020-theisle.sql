f21fb9aea4c9d38688943f8b9b46701cc34700b8
BlackCrystalic <39885003+BlackCrystalic@users.noreply.github.com>
2026-07-20 22:59:05 +0500

    update base to newer version

--- init/020-theisle.sql
+++ init/020-theisle.sql
@@ -56,8 +56,7 @@ ON SEQUENCES TO statistic_component;
 
 
 CREATE TABLE IF NOT EXISTS storage."Dinosaurs" (
-    SteamId   TEXT    NOT NULL,
-    DinoId    INTEGER NOT NULL,
+    DinoId    BIGINT NOT NULL,
     SaveData  TEXT    NOT NULL,
     Active    BOOL    NOT NULL DEFAULT false,
     Died      BOOL    NOT NULL DEFAULT false,
@@ -66,7 +65,7 @@ CREATE TABLE IF NOT EXISTS storage."Dinosaurs" (
 
 CREATE TABLE IF NOT EXISTS storage."LogsDinosaursStorage" (
     Id        BIGSERIAL PRIMARY KEY,
-    DinoId    INTEGER NOT NULL,
+    DinoId    BIGINT NOT NULL,
     Action    TEXT    NOT NULL,
     CreatedAt TIMESTAMPTZ NOT NULL DEFAULT now()
 );
@@ -75,7 +74,7 @@ CREATE INDEX IF NOT EXISTS idx_logs_steamid_dinoid ON storage."LogsDinosaursStor
 
 CREATE TABLE IF NOT EXISTS statistic."Dinosaurs" (
     Id        BIGSERIAL PRIMARY KEY,
-    DinoId    INTEGER NOT NULL,
+    DinoId    BIGINT NOT NULL,
     Growth    FLOAT NOT NULL,
     health    FLOAT NOT NULL,
     Stamina   FLOAT NOT NULL,
@@ -88,6 +87,52 @@ CREATE TABLE IF NOT EXISTS statistic."Dinosaurs" (
     Z         FLOAT NOT NULL,
     CreatedAt TIMESTAMPTZ NOT NULL DEFAULT now()
 );
+
+
+CREATE TABLE IF NOT EXISTS storage."GlobalIDs" (
+    Id BIGSERIAL PRIMARY KEY,
+
+    SteamId  BIGINT NOT NULL,
+    DinoClass  TEXT NOT NULL,
+    DinoId   BIGINT NOT NULL,
+
+    UNIQUE(SteamId, DinoClass, DinoId)
+);
+
+
+CREATE OR REPLACE FUNCTION storage.resolve_dino(
+    p_SteamId BIGINT,
+    p_DinoClass TEXT,
+    p_DinoId BIGINT
+)
+RETURNS BIGINT
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_id BIGINT;
+BEGIN
+    SELECT id
+    INTO v_id
+    FROM storage."GlobalIDs"
+    WHERE SteamId = p_SteamId
+        AND DinoClass = p_DinoClass
+        AND DinoId = p_DinoId;
+
+    IF FOUND THEN
+        RETURN v_id;
+    END IF;
+
+    INSERT INTO storage."GlobalIDs"
+        (SteamId, DinoClass, DinoId)
+    VALUES
+        (p_SteamId, p_DinoClass, p_DinoId)
+
+    RETURNING id
+    INTO v_id;
+
+    RETURN v_id;
+END;
+$$;
 
 
 RESET ROLE;
